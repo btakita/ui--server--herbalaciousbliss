5253ebc5f99545295ebcf699bab3b938ba894836
Brian Takita <brian.takita@gmail.com>
2024-03-13 00:19:11 -0400

    ./layout: layout__doc_html_:
    	- og_image
    	+ favicon?:icon_link_props_T
    	+ social_image_url?:string
    	+ body_class?:string
    	+ ∋ site__description_
    	+ ∋ site__author_
    	+ ∋ site__favicon_
    	+ ∋ site__social_image_url_
    	+ ∋ request_url__origin_
    	+ ∋ site__google_site_verification_
    	+ ∋ site__light_and_dark_mode_
    
    + ∋ @rappstack/domain--server

--- layout/layout__doc_html.ts
+++ layout/layout__doc_html.ts
@@ -1,12 +1,22 @@
-import { import_meta_env_ } from 'ctx-core/env'
+import { request_url__origin_ } from '@rappstack/domain--server/request'
+import {
+	font__meta_props_a1_,
+	type icon_link_props_T,
+	site__author_,
+	site__body_class_,
+	site__description_,
+	site__favicon_,
+	site__google_site_verification_,
+	site__gtag_id_,
+	site__light_and_dark_mode_,
+	site__social_image_url_
+} from '@rappstack/domain--server/site'
 import { class_ } from 'ctx-core/html'
 import { raw_, type tag_dom_T } from 'relementjs'
 import { type tag_props_T } from 'relementjs/any'
 import { body_, head_, link_, meta_, script_, title_ } from 'relementjs/html'
 import { doc_html_ } from 'relementjs/server'
 import { assets_, assets__new, type assets_T, type request_ctx_T, request_url_ } from 'relysjs/server'
-import herbaliciousbliss_logo_webp from '../public/asset/image/herbaliciousbliss-logo.webp'
-const google_site_verification = import_meta_env_().PUBLIC_GOOGLE_SITE_VERIFICATION
 export function layout__doc_html_({
 	ctx,
 	html_props,
@@ -16,7 +26,9 @@ export function layout__doc_html_({
 	title,
 	author,
 	description,
-	og_image,
+	favicon,
+	social_image_url,
+	body_class,
 }:{
 	ctx:request_ctx_T
 	html_props?:tag_props_T&{ class?:string }
@@ -26,15 +38,19 @@ export function layout__doc_html_({
 	title?:string
 	author?:string
 	description?:string
-	og_image?:string
+	favicon?:icon_link_props_T
+	social_image_url?:string
+	body_class?:string
 }, ...children:tag_dom_T[]) {
 	canonical_url ??= request_url_(ctx).href
-	title ??= 'Herbalicious Bliss'
-	description ??= 'Find your Herbal Bliss'
-	author ??= 'Herbalicious Bliss'
-	og_image ??= herbaliciousbliss_logo_webp
-	const social_image_url = new URL(og_image, request_url_(ctx).origin).href
+	description ??= site__description_(ctx)
+	author ??= site__author_(ctx)
+	favicon ??= site__favicon_(ctx)
+	social_image_url = new URL(social_image_url ?? site__social_image_url_(ctx), request_url__origin_(ctx)).href
+	const google_site_verification = site__google_site_verification_(ctx)
+	const site__light_and_dark_mode = site__light_and_dark_mode_(ctx)
 	assets = assets__new(assets_(ctx), assets)
+	const site__gtag_id = site__gtag_id_(ctx)
 	return (
 		doc_html_({
 			...html_props,
@@ -62,16 +78,16 @@ export function layout__doc_html_({
 				meta_({ property: 'twitter:title', content: title }),
 				meta_({ property: 'twitter:description', content: description }),
 				meta_({ property: 'twitter:image', content: social_image_url }),
-				meta_({ name: 'darkreader', content: 'disable' }),
-				// Google Font
-				meta_({ rel: 'preconnect', href: 'https://fonts.googleapis.com' }),
-				meta_({ rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 1 }),
-				meta_({
-					href: 'https://fonts.googleapis.com/css2?family=Urbanist:wght@400;500;600;700&display=swap',
-					rel: 'stylesheet'
-				}),
+				site__light_and_dark_mode
+					? [
+						meta_({ name: 'darkreader-lock' }),
+						meta_({ name: 'darkreader', content: 'disable' }),
+					]
+					: null,
+				font__meta_props_a1_(ctx).map(meta_props=>
+					meta_(meta_props)),
 				meta_({ name: 'theme-color', content: '' }),
-				link_({ rel: 'icon', type: 'image/webp', href: herbaliciousbliss_logo_webp }),
+				link_({ rel: 'icon', ...favicon }),
 				link_({ rel: 'canonical', href: canonical_url }),
 				link_({ rel: 'sitemap', href: '/sitemap.xml' }),
 				google_site_verification
@@ -80,8 +96,10 @@ export function layout__doc_html_({
 					link_({ rel: 'stylesheet', type: 'text/css', href })),
 				...assets.script_a.map(src=>
 					script_({ type: 'module', src })),
-				// language=js
-				script_(raw_(`
+				site__light_and_dark_mode
+					? [
+						// language=js
+						script_({ type: 'module' }, raw_(`
 					// remove fouc
 					const localStorage__theme = localStorage.getItem('theme')
 					document.firstElementChild.setAttribute(
@@ -91,27 +109,41 @@ export function layout__doc_html_({
 							: window.matchMedia('(prefers-color-scheme: dark)').matches
 								? 'dark'
 								: 'light')
-				`.trim().replaceAll('					', ''))),
+				`.trim().replaceAll('					', '')))
+					]
+					: null,
 				title_(title),
 			]),
 			body_({
 				...body__props,
 				class: class_(
+					site__body_class_(ctx),
 					'bg-white',
 					'dark:bg-gray-900',
 					body__props?.class)
 			}, [
-				raw_(`
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-E2YTV44HXX"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-E2YTV44HXX');
-</script>
-				`.trim()),
-				children])
+				children,
+				site__gtag_id
+					? [
+						// Google tag (gtag.js)
+						// language=html
+						script_(raw_(`
+  window.dataLayer = window.dataLayer || []
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date())
+  gtag('config', '${site__gtag_id}')
+  window.addEventListener('load', ()=>{
+		setTimeout(()=>{
+			let script = document.createElement('script')
+			script.src = 'https://www.googletagmanager.com/gtag/js?id=${site__gtag_id}'
+			script.async = true
+			script.defer = true
+			document.head.appendChild(script)
+		}, 1500)
+  })`.trim()))
+					]
+					: null,
+			])
 		])
 	)
 }
