91ee2fa039e344fb3f779caa587c4ded7ed36eca
Brian Takita <brian.takita@gmail.com>
2024-04-03 02:48:55 -0400

    ./layout: layout__doc_html_: ∋ site__author_a1_

--- layout/layout__doc_html.ts
+++ layout/layout__doc_html.ts
@@ -1,8 +1,9 @@
 import { request_url__origin_ } from '@rappstack/domain--server/request'
 import {
+	type author_T,
 	font__meta_props_a1_,
 	type icon_link_props_T,
-	site__author_,
+	site__author_a1_,
 	site__body_class_,
 	site__description_,
 	site__favicon_,
@@ -17,32 +18,34 @@ import { type tag_props_T } from 'relementjs/any'
 import { body_, head_, link_, meta_, script_, title_ } from 'relementjs/html'
 import { doc_html_ } from 'relementjs/server'
 import { assets_, assets__new, type assets_T, type request_ctx_T, request_url_ } from 'relysjs/server'
-export function layout__doc_html_({
-	ctx,
-	html_props,
-	body__props,
-	assets,
-	canonical_url,
-	title,
-	author,
-	description,
-	favicon,
-	social_image_url,
-}:{
+type layout__doc_html_props_T = {
 	ctx:request_ctx_T
 	html_props?:tag_props_T&{ class?:string }
 	body__props?:tag_props_T&{ class?:string }
 	assets?:assets_T
 	canonical_url?:string
 	title?:string
-	author?:string
+	author_a1?:[author_T, ...author_T[]]
 	description?:string
 	favicon?:icon_link_props_T
 	social_image_url?:string
-}, ...children:tag_dom_T[]) {
+}
+export function layout__doc_html_($p:layout__doc_html_props_T, ...children:tag_dom_T[]) {
+	let {
+		ctx,
+		html_props,
+		body__props,
+		assets,
+		canonical_url,
+		title,
+		author_a1,
+		description,
+		favicon,
+		social_image_url,
+	} = $p
 	canonical_url ??= request_url_(ctx).href
 	description ??= site__description_(ctx)
-	author ??= site__author_(ctx)
+	author_a1 ??= site__author_a1_(ctx)!
 	favicon ??= site__favicon_(ctx)
 	social_image_url = new URL(social_image_url ?? site__social_image_url_(ctx), request_url__origin_(ctx)).href
 	const google_site_verification = site__google_site_verification_(ctx)
@@ -64,7 +67,7 @@ export function layout__doc_html_({
 				meta_({ name: 'viewport', content: 'width=device-width' }),
 				meta_({ name: 'title', content: title }),
 				meta_({ name: 'description', content: description }),
-				meta_({ name: 'author', content: author }),
+				meta_({ name: 'author', content: author_a1[0].name }),
 				//  Open Graph / Facebook
 				meta_({ property: 'og:title', content: title }),
 				meta_({ property: 'og:description', content: description }),
